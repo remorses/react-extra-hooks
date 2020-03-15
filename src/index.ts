823c9ed0184a7b1d411cd03874b1dc503dca9885
remorses <beats.by.morse@gmail.com>
2020-03-15 13:20:36 +0100

    index: added useRunWhenDepsReady

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
+import { useEffect } from 'react'
+
 export { usePromise, usePromiseOutput } from './usePromise'
 export { useLazyPromise, useLazyPromiseOutput } from './useLazyPromise'
 export { useDebounce } from './useDebounce'
 export { useLocalStorage } from './useLoaclStorage'
 
 export const sleep = (t) => new Promise((res) => setTimeout(res, t))
+
+export function useRunWhenDepsReady({
+    func,
+    deps = [],
+    undefinedIs = undefined,
+}) {
+    useEffect(() => {
+        const falsy = deps.filter((x) => x === undefinedIs)
+        if (falsy.length) {
+            console.log(
+                `${func.name} still waiting for ${falsy.length} dependencies, ${falsy}`,
+            )
+            return
+        }
+        func(...deps)
+    }, deps)
+}
