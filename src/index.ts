7539c275ede46c073d29bfcb9a0a2994860507fa
remorses <beats.by.morse@gmail.com>
2020-03-19 18:35:36 +0100

    index: default time

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ export function useRunWhenDepsReady({
     }, deps)
 }
 
-export function useInterval({ time, func, args = [] }) {
+export function useInterval({ time=3000, func, args = [] }) {
     useEffect(() => {
         let id = setInterval(() => {
             func(...args)
