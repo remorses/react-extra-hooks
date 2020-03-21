a21f4ad068eb80e8756b894c192a278314ad785d
remorses <beats.by.morse@gmail.com>
2020-03-21 09:26:05 +0100

    useLazyPromise: extracted away makeExecute

--- src/useLazyPromise.ts
+++ src/useLazyPromise.ts
@@ -53,77 +53,77 @@ export type useLazyPromiseOutput<Arguments extends any[], ResultType> = [
 
 export function useLazyPromise<Arguments extends any[], ResultType = any>(
     promise: (...x: Arguments) => Promise<ResultType> | null | undefined,
-    {
-        cache = false,
-        promiseId,
-        cacheExpirationSeconds = 120,
-        cacheSize = 50,
-    } = {} as CacheaOptions,
+    cacheOptions = {} as CacheaOptions,
 ): useLazyPromiseOutput<Arguments, ResultType> {
-    promiseId = promiseId || promise?.name
+    cacheOptions.promiseId = cacheOptions.promiseId || promise?.name
     const [{ error, result, loading }, dispatch] = useReducer(reducer, {
         error: undefined,
         result: undefined,
         state: states.pending,
     })
     useEffect(() => {
-        if (promise && cache && !promiseId) {
+        if (promise && cacheOptions.cache && !cacheOptions.promiseId) {
             console.error(
                 'ERROR react-extra-hooks useLazyPromise should receive either a promiseId or a name function!\ncache will not be hashed correctly',
             )
             console.log('for function ' + promise.toString())
         }
-    }, [promiseId, cache])
+    }, [cacheOptions.promiseId, cacheOptions.cache])
     const execute = useCallback(
-        (...args: Arguments) => {
-            if (!promise) {
-                throw new Error(
-                    `promise is null or undefined, cannot execute with args '${args}'`,
-                )
-            }
-
-            if (cache) {
-                let hit = getFromCache({ promiseId, args })
-                if (hit) {
-                    // console.log('cache hit for ' + JSON.stringify(hit))
-                    dispatch({
-                        payload: hit,
-                        type: states.resolved,
-                    })
-                    return Promise.resolve(hit)
-                }
-            }
-            dispatch({ type: states.pending })
-            return promise(...args)
-                .then((result) => {
-                    if (cache) {
-                        updateCache({
-                            promiseId,
-                            args,
-                            cacheExpirationSeconds,
-                            cacheSize,
-                            result,
-                        })
-                    }
-                    dispatch({
-                        payload: result,
-                        type: states.resolved,
-                    })
-                    return result
-                })
-                .catch((error) => {
-                    dispatch({
-                        payload: error,
-                        type: states.rejected,
-                    })
-                    throw error
-                })
-        },
+        makeExecute({ cacheOptions, promise, dispatch }),
         [!promise, dispatch, result],
     )
     const invalidate = () =>
         clearMemoryCache({
-            promiseId,
+            promiseId: cacheOptions.promiseId,
         })
     return [execute, { result, error, loading }, invalidate]
+}
+
+function makeExecute({ promise, cacheOptions, dispatch }) {
+    return (...args) => {
+        if (!promise) {
+            throw new Error(
+                `promise is null or undefined, cannot execute with args '${args}'`,
+            )
+        }
+
+        if (cacheOptions.cache) {
+            let hit = getFromCache({ promiseId: cacheOptions.promiseId, args })
+            if (hit) {
+                // console.log('cache hit for ' + JSON.stringify(hit))
+                dispatch({
+                    payload: hit,
+                    type: states.resolved,
+                })
+                return Promise.resolve(hit)
+            }
+        }
+        dispatch({ type: states.pending })
+        return promise(...args)
+            .then((result) => {
+                if (cacheOptions.cache) {
+                    updateCache({
+                        promiseId: cacheOptions.promiseId,
+                        args,
+                        cacheExpirationSeconds:
+                            cacheOptions.cacheExpirationSeconds,
+                        cacheSize: cacheOptions.cacheExpirationSeconds,
+                        result,
+                    })
+                }
+                dispatch({
+                    payload: result,
+                    type: states.resolved,
+                })
+                return result
+            })
+            .catch((error) => {
+                dispatch({
+                    payload: error,
+                    type: states.rejected,
+                })
+                throw error
+            })
+    }
 }
